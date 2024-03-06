5245ecf82161442cb61d8652340957719e3c8f30
vandressa <vandressa.soares@splab.ufcg.edu.br>
2024-03-06 19:18:31 -0300

    criando teste gerador nota fiscal serviço treinamento

--- gerador-nota-fiscal/gerador-nota-fiscal/src/test/java/GeradorNotaFiscalTest.java
+++ gerador-nota-fiscal/gerador-nota-fiscal/src/test/java/GeradorNotaFiscalTest.java
@@ -20,4 +20,15 @@ public class GeradorNotaFiscalTest {
         assertEquals(100.0, notaFiscal.getValor());
         assertEquals(25.0, notaFiscal.getImposto());
     }
+
+
+    @Test
+    public void testGerarNotaFiscalParaServicoDeTreinamento(){
+        Fatura fatura = new Fatura("Cliente Teste", "Endereço Teste", "TREINAMENTO", 100.0);
+        GeradorNotaFiscal gerador = new GeradorNotaFiscal();
+        NotaFiscal nf = gerador.gerarNotaFiscal(fatura);
+        assertEquals("Cliente Teste", nf.getCliente());
+        assertEquals(100.0, nf.getValor());
+        assertEquals(15.0, nf.getImposto());
+    }
 }
