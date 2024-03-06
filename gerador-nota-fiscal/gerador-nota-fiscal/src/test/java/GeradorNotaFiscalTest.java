61d86fc6e7920202b6b47d9a12439958ebc6140c
vandressa <vandressa.soares@splab.ufcg.edu.br>
2024-03-06 18:56:49 -0300

    criando teste gerador nota fiscal serviço consultoria

--- gerador-nota-fiscal/gerador-nota-fiscal/src/test/java/GeradorNotaFiscalTest.java
+++ gerador-nota-fiscal/gerador-nota-fiscal/src/test/java/GeradorNotaFiscalTest.java
@@ -4,7 +4,15 @@ import org.junit.jupiter.api.Test;
 public class GeradorNotaFiscalTest {
 
     @Test
-    public void testEmpty() {
-        Assertions.assertEquals(0, 1);
+    public void testGerarNotaFiscalServicoConsultoria() {
+
+        Fatura fatura = new Fatura("Cliente Teste", "Endereço Teste", "CONSULTORIA", 100.0);
+        GeradorNotaFiscal gerador = new GeradorNotaFiscal();
+        NotaFiscal notaFiscal = gerador.gerarNotaFiscal(fatura);
+
+
+        assertEquals("Cliente Teste", notaFiscal.getNomeCliente());
+        assertEquals(100.0, notaFiscal.getValor(), 100.0);
+        assertEquals(250.0, notaFiscal.getImposto(),25.0);
     }
 }
