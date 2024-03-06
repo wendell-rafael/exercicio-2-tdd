cada35fd49ad12ea1f37ab624a659b65af22be79
Wendell Rafael <wendell.nascimento@splab.ufcg.edu.br>
2024-03-06 12:13:11 -0300

    fazendo codigo para passar no teste de criar tarefa

--- gerenciador-de-tarefas/gerenciador-tarefas/src/main/java/org/example/GerenciadorTarefas.java
+++ gerenciador-de-tarefas/gerenciador-tarefas/src/main/java/org/example/GerenciadorTarefas.java
@@ -1,12 +1,21 @@
 package org.example;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class GerenciadorTarefas {
+    private List<Tarefa> listaTarefas;
+
+    public GerenciadorTarefas() {
+        this.listaTarefas = new ArrayList<>();
+    }
+
     public void criarTarefa(String titulo, String descricao, String dataVencimento, Prioridade prioridade) {
+        Tarefa tarefa = new Tarefa(listaTarefas.size(), titulo, descricao, dataVencimento, prioridade);
+        listaTarefas.add(tarefa);
     }
 
     public List<Tarefa> getTarefas() {
-        return null;
+        return listaTarefas;
     }
 }
