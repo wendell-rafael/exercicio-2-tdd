43ddf7b91331cc5cc82d67c90d05f3c389eed6c7
Wendell Rafael <wendell.nascimento@splab.ufcg.edu.br>
2024-03-06 13:08:28 -0300

    commit inicial

---
+++ gerador-nota-fiscal/gerador-nota-fiscal/src/test/java/GeradorNotaFiscalTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class GeradorNotaFiscalTest {
+
+    @Test
+    public void testEmpty() {
+        Assertions.assertEquals(0, 1);
+    }
+}
